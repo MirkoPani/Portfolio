ae5f3eb743fbbc1c59f08aa89ee294849c485632
Mirko Pani <panimirko95@gmail.com>
2021-09-15 23:03:18 +0200

    Updated about page and added social icons on footer

---
+++ components/Social.vue
@@ -0,0 +1,23 @@
+<template>
+	<a :href="url" target="_blank">
+		<i v-bind:class="{
+			'bg-yellow-200 hover:bg-yellow-500': cv, 
+			'bg-green-200 hover:bg-green-400' : !cv,
+			}" 
+		class="shadow-lg font-lg p-3 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block text-center">
+		<slot></slot>
+		</i>
+	</a>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+	props: {
+		url : String,
+		icon : String,
+		cv : Boolean
+	}
+})
+</script>
+
--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
 <div>
 	<PageHero :heroTitle="heroTitle"/>
 	<section class="container leading-relaxed mx-auto py-4 text-gray-300 text-left md:px-4 flex lg:flex-row flex-col gap-4">
-		<div class="order-2 lg:order-1 lg:w-2/3">
+		<div class="order-2 lg:order-1 lg:w-2/3 flex flex-col">
 			<p >Mi chiamo <TextHighlight>Mirko Pani</TextHighlight>, sono un giovane sviluppatore di 26 anni residente nella provincia di <TextHighlight>Vicenza.</TextHighlight></p> 
 			<p>Oltre ad essere un forte <TextHighlight>appassionato</TextHighlight> del mondo informatico, mi diletto nel campo della fotografia e dei primi piani.</p>
 			<p>Quando non sono occupato a viaggiare e a fare foto, mi troveresti spesso a leggere un libro tecnico per apprendere un nuovo concetto.</p>
@@ -19,6 +19,9 @@
 				<li>Wordpress</li>
 			</ul>
 			<p class="mt-10">Se invece sei curioso di scoprire di più sul mio lato da diletto fotografo, puoi vedere le mie foto <a href="https://www.instagram.com/mirkoopani" target="_blank" ><TextHighlight class="underline hover:text-green-600">qui</TextHighlight></a> e <a href="https://www.instagram.com/mirkoophoto" target="_blank" ><TextHighlight class="underline hover:text-green-600">qui</TextHighlight></a>.</p>
+		
+			<a href="" target="_blank" class="text-center mt-10 border md:w-1/3 self-center border-green-300 p-4 rounded-lg text-green-300"><button >Visualizza il mio curriculum</button></a>
+		
 		</div>
 		<div class="order-1 lg:order-2 lg:w-1/3">
 			<img class="rounded-md h-80 lg:h-96 mx-auto" src="~/assets/img/me.jpg" alt="a personal portrait of myself" />
@@ -29,7 +32,7 @@
 		<div>
 			<div class="py-4">
 				<p class="text-xl">Sviluppatore @ <TextHighlight><a target="_blank" href="https://www.es-tek.it/">Es-Tek</a></TextHighlight></p>
-				<ul class="list-outside">
+				<ul class="list-outside mt-2">
 					<li>Lavorato con un team internazionale su software già esistenti, integrando nuove funzionalità e bug fix</li>
 					<li>Utilizzato una moltitudine di linguaggi, piattaforme e framework in ambito client: <TechnologyHighlight>C++</TechnologyHighlight>, <TechnologyHighlight>OpenCV</TechnologyHighlight>, <TechnologyHighlight>Qt</TechnologyHighlight>, <TechnologyHighlight>C#</TechnologyHighlight>, <TechnologyHighlight>WPF</TechnologyHighlight></li>
 					<li>Utilizzato una moltitudine di linguaggi, piattaforme e framework in ambito web: <TechnologyHighlight>Wordpress</TechnologyHighlight>, <TechnologyHighlight>C#</TechnologyHighlight>, <TechnologyHighlight>Asp.Net Core</TechnologyHighlight>, <TechnologyHighlight>EF Core</TechnologyHighlight>, <TechnologyHighlight>Blazor</TechnologyHighlight>, <TechnologyHighlight>HTML e CSS</TechnologyHighlight></li>
@@ -40,7 +43,7 @@
 			</div>
 			<div class="py-4">
 				<p class="text-xl">Tirocinio @ <TextHighlight><a target="_blank" href="https://www.fbk.eu/it/">Fondazione Bruno Kessler (FBK)</a></TextHighlight></p>
-				<ul class="list-inside">
+				<ul class="list-inside mt-2">
 					<li>Studio e realizzazione tesi di laurea sotto la supervisione di un ricercatore</li>
 					<li>Implementato un proof of concept per la trasmissione di dati ottenuti da un dispositivo Leap Motion in un ambiente a realtà virtuale (VR) multiutente su smartphone.</li>
 					<li>Dai risultati ottenuti, collaborato alla stesura di un paper scientifico in inglese, discusso ad una conferenza e successivamente pubblicato in una rivista, dal titolo <TextHighlight><a class="underline" target="_blank" href="https://www.researchgate.net/publication/326370856_Distributed_Data_Exchange_with_Leap_Motion_5th_International_Conference_AVR_2018_Otranto_Italy_June_24-27_2018_Proceedings_Part_II">"Distributed Data Exchange with Leap Motion"</a></TextHighlight></li>
@@ -49,14 +52,14 @@
 			</div>
 			<div class="py-4">
 				<p class="text-xl">Laurea in Informatica @ <TextHighlight>Università degli Studi di Trento</TextHighlight></p>
-				<ul class="list-inside">
+				<ul class="list-inside mt-2">
 					<li>Apprendimento di concetti come: Agile, Unit test, OOP, algoritmi e strutture dati, Linux</li>
 					<li>Esperienza in: <TechnologyHighlight>Android</TechnologyHighlight>, <TechnologyHighlight>Javascript</TechnologyHighlight>, <TechnologyHighlight>C++</TechnologyHighlight>, <TechnologyHighlight>SQL</TechnologyHighlight>, <TechnologyHighlight>Java</TechnologyHighlight></li>
 				</ul>
 			</div>
 			<div class="py-4">
 				<p class="text-xl">Diploma 'Perito Capotecnico' @ <TextHighlight>Istituto Tecnico Tecnologico G. Chilesotti</TextHighlight></p>
-				<ul class="list-inside">
+				<ul class="list-inside mt-2">
 					<li>Esperienza in: <TechnologyHighlight>C#</TechnologyHighlight>, <TechnologyHighlight>Windows Form</TechnologyHighlight>, <TechnologyHighlight>SQL</TechnologyHighlight>, <TechnologyHighlight>Java</TechnologyHighlight></li>
 				</ul>
 			</div>
